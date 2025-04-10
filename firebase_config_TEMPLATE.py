8d775b4f02943f83b7bf4e89ca9eac321e3d0f66
Eneias Da Fonseca Dias <Eneias.Dias@sicoob.com.br>
2025-04-09 23:41:10 -0300

    Configuração com variáveis de ambiente do Firebase

--- firebase_config_TEMPLATE.py
+++ firebase_config_TEMPLATE.py
@@ -1,9 +1,11 @@
+import os
+
 firebase_config = {
-    "apiKey": "",
-    "authDomain": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "databaseURL": ""
+    "apiKey": os.environ.get("FIREBASE_API_KEY"),
+    "authDomain": os.environ.get("FIREBASE_AUTH_DOMAIN"),
+    "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.environ.get("FIREBASE_MSG_SENDER_ID"),
+    "appId": os.environ.get("FIREBASE_APP_ID"),
+    "databaseURL": os.environ.get("FIREBASE_DB_URL")
 }
